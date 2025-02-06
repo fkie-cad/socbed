7cda031a154672a47dd93352537b53d1c1c82adf
Philipp Boenninghausen <philipp.schlipper@fkie.fraunhofer.de>
2025-02-06 09:07:12 +0100

    Remove deprecated code

--- src/systests/test_logging.py
+++ src/systests/test_logging.py
@@ -34,7 +34,7 @@ from vmcontrol.sessionhandler import SessionHandler
 from vmcontrol.vmmcontroller import VBoxController
 
 MAX_RUNTIME = 10 * 60  # Ten minutes
-pytestmark = [pytest.mark.systest, pytest.mark.tmp_logging]
+pytestmark = [pytest.mark.systest]
 
 
 @pytest.fixture(scope="module")
