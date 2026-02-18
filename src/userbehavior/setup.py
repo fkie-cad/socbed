ea1fc13f3fd93281dc8a6329b4d868f7e52fbd55
Philipp Boenninghausen <philipp.schlipper@fkie.fraunhofer.de>
2026-02-18 16:42:44 +0100

    Fix selenium used in unit tests to 4.3 (same as in requirements)

--- src/userbehavior/setup.py
+++ src/userbehavior/setup.py
@@ -5,10 +5,10 @@ setup(
     version="1.0.3",
     packages=find_packages(exclude=["*tests"]),
     package_data={"userbehavior.browsing": ["firefox_extensions/*.xpi"]},
-    install_requires=["selenium>=3.0.0"],
+    install_requires=["selenium==4.3.0"],
     entry_points={
-        'console_scripts': [
-            'userbehavior = userbehavior.run:main',
+        "console_scripts": [
+            "userbehavior = userbehavior.run:main",
         ]
-    }
+    },
 )
